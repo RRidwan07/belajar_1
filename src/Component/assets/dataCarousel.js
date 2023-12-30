b34beda67d51049de45c6e99c70ec76526e61ecd
Sahlan soppiyan <sahlansoppiyan@gmail.com>
2023-12-30 16:19:14 +0700

    news images

--- src/Component/assets/dataCarousel.js
+++ src/Component/assets/dataCarousel.js
@@ -19,5 +19,20 @@ const dataCarousel = [
         images : '/img/hr.jpg',
         descripsi : '......',
     },
+    {
+        id : 4,
+        images : '/img/g3.jpg',
+        descripsi : '......',
+    },
+    {
+        id : 5,
+        images : '/img/g4.jpg',
+        descripsi : '......',
+    },
+    {
+        id : 6,
+        images : '/img/g5.jpg',
+        descripsi : '......',
+    },
 ]
 export default dataCarousel
